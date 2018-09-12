8209c0f73ebc7b2a8b78ad2eea24f40891527f46
KU <ku@digitaldolphins.jp>
2018-09-12 14:59:49 +0900

    0.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace MAPIClientSelector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flow = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flow
+            // 
+            this.flow.AutoScroll = true;
+            this.flow.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flow.Location = new System.Drawing.Point(0, 0);
+            this.flow.Name = "flow";
+            this.flow.Size = new System.Drawing.Size(700, 337);
+            this.flow.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(700, 337);
+            this.Controls.Add(this.flow);
+            this.Font = new System.Drawing.Font("游ゴシック", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MAPI Client Selector";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flow;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using MAPIClientSelector.Properties;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MAPIClientSelector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var mail = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Clients\Mail", false);
+            if (mail != null)
+            {
+                foreach (var _key in mail.GetSubKeyNames())
+                {
+                    var key = _key;
+                    var keyKey = mail.OpenSubKey(key, false);
+                    if (keyKey != null)
+                    {
+                        var text = keyKey.GetValue(null) + "";
+                        Button button = new Button
+                        {
+                            AutoSize = true,
+                            AutoSizeMode = AutoSizeMode.GrowAndShrink,
+                            Image = Resources.Next_16x,
+                            TextImageRelation = TextImageRelation.ImageBeforeText,
+                            Text = text,
+                        };
+                        button.Click += (a, b) =>
+                        {
+                            Registry.SetValue(@"HKEY_CURRENT_USER\SOFTWARE\Clients\Mail", "", key);
+                            MessageBox.Show(this, $"{text} に変更しました。", Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            Close();
+                        };
+                        flow.Controls.Add(button);
+                        flow.SetFlowBreak(button, true);
+                    }
+                }
+            }
+        }
+    }
+}
